38bf1f11935b8423e4966106e5576d39cc22235d
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-09-13 03:53:33 -0500

    Browser setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./features/homepage/Homepage";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import OrderId from "./features/order id/OrderId";
+import Error from "./ui/Error/Error";
+
 function App() {
-  return <p>sdfds</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="menu" element={<Menu />}>
+          <Route path="cart" element={<Cart />} />
+        </Route>
+        <Route path="order" element={<Order />} />
+        <Route path="order/:id" element={<OrderId />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
