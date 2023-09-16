66162152e65e1bd72abbb068119275e0bfb4ff20
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-09-15 22:37:36 -0500

    cart slice --- add actions

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -17,3 +17,16 @@ const cartSlice = createSlice({
 export const { addPizzaToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+// {
+//   "id": 1,
+//   "name": "Margherita",
+//   "unitPrice": 12,
+//   "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-1.jpg",
+//   "ingredients": [
+//   "tomato",
+//   "mozzarella",
+//   "basil"
+//   ],
+//   "soldOut": false,
+// },
